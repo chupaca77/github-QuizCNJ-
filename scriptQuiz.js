c31a7bb6eab660f757ddcd6ac1efb91e2389cd7e
chupaca77 <jlespinasse69@gmail.com>
2025-02-17 14:43:40 +0100

    fichier script pré-rempli

---
+++ scriptQuiz.js
@@ -0,0 +1,25 @@
+export const quiz_fatoumata_kebe = {
+    questions: [
+      {
+        text: "Fatoumata Kébé est reconnue pour ses recherches dans quel domaine scientifique ?",
+        options: [
+          "L'astronomie, spécialisée dans les débris spatiaux.",
+          "La création de lunettes de réalité augmentée pour observer les étoiles.",
+          "L'étude de la météo sur Mars et ses implications pour l'agriculture spatiale.",
+          "La cartographie des astéroïdes qui pourraient potentiellement entrer en collision avec la Terre."
+        ],
+        correct_answer: "L'astronomie, spécialisée dans les débris spatiaux."
+      },
+      {
+        text: "En plus de son travail d'astronome, dans quelle autre activité Fatoumata Kébé est-elle impliquée ?",
+        options: [
+          "Elle enseigne l'astrophotographie aux astronautes de la Station spatiale internationale.",
+          "Elle milite pour l'éducation des jeunes filles en astronomie.",
+          "Elle entraîne des robots pour des compétitions de quiz interstellaires.",
+          "Elle développe des applications pour cultiver des potagers en apesanteur."
+        ],
+        correct_answer: "Elle milite pour l'éducation des jeunes filles en astronomie."
+      },
+      // Autres questions...
+    ]
+  };
